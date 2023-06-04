28c2896c2a3d6064d373618cbec4e84439cb3a9e
carveyh <carvey.hor@gmail.com>
2023-06-04 16:15:36 -0400

    Fix issue with auth modal not appearing from profile button

--- sparebnb/frontend/src/components/LoginFormModal/index.js
+++ sparebnb/frontend/src/components/LoginFormModal/index.js
@@ -1,7 +1,6 @@
 import LoginForm from "./LoginForm";
 import { useState } from "react";
 import { Modal } from "../../context/Modal";
-// import { NavLink } from "react-router-dom";
 
 const LoginFormModal = () => {
 	const [showModal, setShowModal] = useState(false);
@@ -9,8 +8,6 @@ const LoginFormModal = () => {
 	return (
 		<>
 			<div onClick={e => setShowModal(true)}>Log in</div>
-			{/* <NavLink onClick={e => setShowModal(true)} to="">Log In</NavLink> */}
-			{/* <button onClick={e => setShowModal(true)}>Log In</button> */}
 			{showModal && <Modal onClose={e => setShowModal(false)}>
 				<LoginForm />
 			</Modal>}
