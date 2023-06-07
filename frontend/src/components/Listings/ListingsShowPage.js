8985db44a390ea8a51bd6093992fb4d0c2ee4365
carveyh <carvey.hor@gmail.com>
2023-06-07 03:23:39 -0400

    Show page dynamic resizing basic working for full page

--- frontend/src/components/Listings/ListingsShowPage.js
+++ frontend/src/components/Listings/ListingsShowPage.js
@@ -1,43 +1,85 @@
 import "./ListingsShowPage.css";
 
-const ListingsShowPage = () => {
-	return (
+export const ListingsShowPhoto = (folderNum, imageNum) => {
+	return(
 		<>
-			<div className="show-header-full-header">
-				<div className="show-header-inner-header">
-					<div className="show-title">
-					Lake Tent #4 @ The Silverlaken Estate
-					</div>
-					<div className="show-header-details">
-						<div className="show-header-stats">
-							<span className="rating-review-stats">
-								<span className="star-icon"><i class="fa-solid fa-star"></i></span>
-								<span className="header-rating">4.93 ·</span>
-								<span className="header-review-count">15 reviews</span>
-							</span>
+			<img className="listings-show-photo" src={require(`../../images/listings/02/01.png`)} />
+
+		</>
+		
+	)
+}
+
+const ListingsShowPage = () => {
+	const photoDirPath = "";
+	
+	return (
+		<div className="show-page-outer-container">
+			<div className="show-page-dynamic-inner-container">
+				{/* HEADER START */}
+				{/* HEADER START */}
+				<div className="show-header-full-header">
+					<div className="show-header-inner-header">
+						<div className="show-title">
+						Lake Tent #4 @ The Silverlaken Estate
 						</div>
-						<div className="show-header-buttons">Share Save</div>
+						<div className="show-header-details">
+							<div className="show-header-stats">
+								<span className="rating-review-stats">
+									<span className="star-icon"><i class="fa-solid fa-star"></i></span>
+									<span className="header-rating">4.93 ·</span>
+									<span className="header-review-count">15 reviews</span>
+								</span>
+							</div>
+							<div className="show-header-buttons">Share Save</div>
+						</div>
 					</div>
 				</div>
-			</div>
+				{/* HEADER END */}
+				{/* HEADER END */}
 
-			{/* PHOTO WALL */}
-			<div className="photo-wall-padding">
-				<div className="photo-wall-container">
-					<div className="photo-wall-halver">
-						<div className="photo-wall-item photo-wall-big"></div>
-						<div className="photo-wall-grid">
-							<div className="photo-wall-item photo-wall-one">ONE</div>
-							<div className="photo-wall-item photo-wall-two">TWO</div>
-							<div className="photo-wall-item photo-wall-three">THREE</div>
-							<div className="photo-wall-item photo-wall-four">FOUR</div>
+				{/* PHOTO WALL START */}
+				{/* PHOTO WALL START */}
+				<div className="photo-wall-crown">
+					<div className="photo-wall-container">
+						<div className="photo-wall-halver">
+							<div className="photo-wall-photo-container photo-wall-big"><ListingsShowPhoto/></div>
+							<div className="photo-wall-grid">
+								<div className="photo-wall-photo-container photo-wall-one">
+									ONE
+								</div>
+								<div className="photo-wall-photo-container photo-wall-two">
+									TWO
+								</div>
+								<div className="photo-wall-photo-container photo-wall-three">
+									THREE
+								</div>
+								<div className="photo-wall-photo-container photo-wall-four">
+									FOUR
+								</div>
 
+							</div>
 						</div>
-					</div>
-					
-				</div>	
+						
+					</div>	
+				</div>
+				{/* PHOTO WALL END */}
+				{/* PHOTO WALL END */}
+				<br/>
+				<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
+				<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
+				<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
+				<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
+				<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
+
+
+
+
+
+
+
 			</div>
-		</>
+		</div>
 	)
 }
 
---
+++ frontend/src/images/image_sources.md
@@ -0,0 +1,22 @@
+Folder | Airbnb Title | airbnb.com/rooms/:room_id
+
+01 "Beautiful riverfront mid-century house" 578897320179146437
+02 "Lake Tent #4 @ The Silverlaken Estate" 653805328196091145
+03 " 
+04 " 
+05 " 
+06 " 
+07 " 
+08 " 
+09 " 
+10 " 
+11 " 
+12 " 
+13 " 
+14 " 
+15 " 
+16 " 
+17 " 
+18 " 
+19 " 
+20 " 
