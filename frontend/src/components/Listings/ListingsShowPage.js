e83969ba425d1732f04638d42559be3c5eabe43d
carveyh <carvey.hor@gmail.com>
2023-06-07 14:05:53 -0400

    Show page global styles refactor, details stats card

--- frontend/src/components/Listings/ListingsShowPage.js
+++ frontend/src/components/Listings/ListingsShowPage.js
@@ -20,18 +20,22 @@ const ListingsShowPage = () => {
 				{/* HEADER START */}
 				<div className="show-header-full-header">
 					<div className="show-header-inner-header">
-						<div className="show-title">
+						<div className="show-title heading-1">
 						Lake Tent #4 @ The Silverlaken Estate
 						</div>
 						<div className="show-header-details">
 							<div className="show-header-stats">
-								<span className="rating-review-stats">
+								<span className="rating-review-stats stats-text-small">
 									<span className="star-icon"><i class="fa-solid fa-star"></i></span>
 									<span className="header-rating">4.93 ·</span>
 									<span className="header-review-count">15 reviews</span>
 								</span>
 							</div>
-							<div className="show-header-buttons">Share Save</div>
+							<div className="show-header-buttons stats-text-small">
+								<i class="fa-solid fa-arrow-up-from-bracket"></i>&nbsp;&nbsp;Share 
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<i class="fa-regular fa-heart"></i>&nbsp;&nbsp;Save
+							</div>
 						</div>
 					</div>
 				</div>
@@ -97,6 +101,46 @@ const ListingsShowPage = () => {
 				<div className="details-outer-container">
 					<div className="details-main-container">
 						<div className="details-left-container">
+							<div className="details-stats-card-container">
+								<div className="details-stats-card-padder">
+									<div className="details-stats-card-horizontal-splitter">
+										<div className="details-stats-card-text-container">
+												<div className="details-stats-card-text-top heading-2">
+													Entire home hosted by Garret
+												</div>
+												<div className="details-stats-card-text-bottom plain-text">
+													10 guests · 5 bedrooms · 6 beds · 3.5 baths
+												</div>
+										</div>
+										<div className="details-stats-card-profile-thumbnail">Prof pic</div>
+									</div>
+								</div>
+								<div className="details-stats-card-padder details-horizontal-rule-top-border">
+									Sup
+								</div>
+							</div>
+
+
+
+
+
+
+
+
+
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
+							<br/>
 							<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
 							<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
 							<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
@@ -158,6 +202,15 @@ const ListingsShowPage = () => {
 							<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
 							<div>afawei aowefjaowijefaowief oawjiefawojiefjiwae aowefjaowiefijawj e</div>
 						</div>
+
+
+
+
+
+
+
+
+
 						<div className="details-right-container">
 							<div className="floating-form-container">
 								<div className="floating-form-inner-container">
