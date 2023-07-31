c6a69f38ff366a51dd24d63fe6f88a6bf738c8e1
carveyh <carvey.hor@gmail.com>
2023-07-31 11:45:32 -0400

    Basic styling for map popup

--- frontend/src/components/SpareMap/MapInfoCard.js
+++ frontend/src/components/SpareMap/MapInfoCard.js
@@ -3,15 +3,31 @@ import { Link } from "react-router-dom";
 import { formatTwoDigitNumberString } from "../../utils/urlFormatter";
 
 export const MapInfoCard = ({listing}) => {
+	const formattedOverallRating = () => {
+		const twoDigit = listing?.averageRatings.overallRating.toFixed(2)
+		const oneDigit = listing?.averageRatings.overallRating.toFixed(1)
+		return (twoDigit === oneDigit + '0') ? oneDigit : twoDigit;
+	}
+
 	return(
 		<Link to={`/listings/${listing?.id}`}>
 		<div className="map-info-card-container">
 			<div className="map-info-title">{listing.title}</div>
+			<div className="map-info-location">{`${listing.city}, ${listing.state}`}</div>
+			<div className="map-info-pricing-rating">
+				<span>{`$${listing.baseNightlyRate}`}</span>
+				<span>{listing.numRatings >=3 && 
+					<span className="map-rating-box">
+						<span className="star-icon-index-map"><i className="fa-solid fa-star"></i></span>
+						<span>{`${formattedOverallRating()}`}</span>
+					</span>
+				}
+				</span>
+			</div>
 			<div className="listings-photo-container">
-							{/* <img className="listings-photo" src={require(`../../images/listings/${photoFileNames[num-1]}.png`)} /> */}
-							<div className="hover-overlay"></div>
-							<img className="listings-photo" src={require(`../../images/listings/${formatTwoDigitNumberString(listing?.id)}/01.png`)} />
-						</div>
+				<div className="hover-overlay"></div>
+				<img className="listings-photo" src={require(`../../images/listings/${formatTwoDigitNumberString(listing?.id)}/01.png`)} />
+			</div>
 		</div>
 		</Link>
 	)
